7a84a5a4f4ede14a8de27dabcd897315783a6bb1
aromarich21 <aromarich21@yandex.ru>
2020-05-04 16:35:45 +0300

    v 0.7.3 frontend

--- frontend-service/Pages/Index.cshtml.cs
+++ frontend-service/Pages/Index.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using SHDocVw;
 
 namespace frontend_service.Pages
 {
@@ -12,11 +13,17 @@ namespace frontend_service.Pages
     {
         private readonly ILogger<IndexModel> _logger;
         public string _cookieAccept;
-        public bool point;
+        public string hidden;
 
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
+        }
+
+        public async Task OnPostAsync() //onclick submit
+        {
+            CookieAdd();
+            Response.Redirect("/Index");
         }
 
         public void OnGet()
@@ -25,16 +32,27 @@ namespace frontend_service.Pages
             CookiesJob();
         }
 
-        public void CookiesJob() //job with cookie
+        public void CookieAdd() //job with token
         {
-            _cookieAccept = Request.Cookies["token_"];
+            _cookieAccept = Request.Cookies["_cookieAccept"];
             if (_cookieAccept == null || _cookieAccept == "null" || _cookieAccept == "")
             {
-                point = true;
+                _cookieAccept = Guid.NewGuid().ToString();  //UUID generate
+                //token_ = TokenGenerate(5); //token value generate
+                Response.Cookies.Append("_cookieAccept", _cookieAccept);   //cookie add [_cookieAccept]
+            }
+        }
+
+        public void CookiesJob() //job with cookie
+        {
+            _cookieAccept = Request.Cookies["_cookieAccept"];
+            if (_cookieAccept == null || _cookieAccept == "null" || _cookieAccept == "")
+            {
+                hidden = "";
             }
             else
             {
-                point = false;
+                hidden = "hidden";
             }
         }
     }
--- frontend-service/Startup.cs
+++ frontend-service/Startup.cs
@@ -10,7 +10,7 @@ namespace frontend_service
 {
     public class Startup
     {
-        static public string version = "0.7.2";
+        static public string version = "0.7.3";
         static public string versionShow = "v " + version + " alpha";
         static public List<ClientDataFront> clientData;
         static public QAdata qaData = new QAdata();
