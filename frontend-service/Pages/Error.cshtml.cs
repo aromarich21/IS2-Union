8db4811facf0489cb91532e32df440a3db7afe39
aromarich21 <aromarich21@yandex.ru>
2020-05-26 20:29:18 +0300

    utf8 encode

--- frontend-service/Pages/Error.cshtml.cs
+++ frontend-service/Pages/Error.cshtml.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.Text;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -12,6 +13,8 @@ namespace frontend_service.Pages
         static public string ErrorMessage { get; set; }
         public string TitleError = "ОШИБКА";
         static public string DefaultMessage = "Упс, произошла непредвиденная ошибка. Пожалуйста, попробуйте еще раз.";
+        public string Message { get; set; }
+        public string Title { get; set; }
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
         private readonly ILogger<ErrorModel> _logger;
         public ErrorModel(ILogger<ErrorModel> logger)
@@ -24,8 +27,16 @@ namespace frontend_service.Pages
             if (this.HttpContext.Response.StatusCode == 404)
             {
                 TitleError = "404";
-                ErrorMessage = "Запрашивая страница не найдена!";
+                ErrorMessage = "Запрашиваемая страница не найдена!";
             }
+            if (ErrorMessage == null || ErrorMessage == "")
+            {
+                ErrorMessage = DefaultMessage;
+            }
+            byte[] utf8title = Encoding.UTF8.GetBytes(TitleError);
+            byte[] utf8message = Encoding.UTF8.GetBytes(ErrorMessage);
+            Message = Encoding.UTF8.GetString(utf8message);
+            Title = Encoding.UTF8.GetString(utf8title);
         }     
     }
 }
